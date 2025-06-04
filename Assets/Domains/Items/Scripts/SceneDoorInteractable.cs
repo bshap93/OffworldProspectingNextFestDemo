af1a6061ec315d0fb572a1c690e3d503632f9a0e
Ben Shapiro <bshap93@gmail.com>
2025-06-04 14:33:56 -0600

    Changes?

---
+++ Assets/Domains/Items/Scripts/SceneDoorInteractable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace Domains.Items.Scripts
+{
+    public class SceneDoorInteractable : InteractableObjective
+    
+    {
+        public override void Interact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IEnumerator InitializeAfterProgressionManager()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
